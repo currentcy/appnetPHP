670790cc602310b30980e6dbf0f491e3e829a1dd
chris <chris@kentkinetic.com>
2013-02-18 17:26:56 -0600

    Input into DB
    Currently it is searching for the hashtag #mondaynightdanceparty. It is
    taking all those who use it, and put it into a db.

--- appnetvon.php
+++ appnetvon.php
@@ -1,3 +1,4 @@
+                                
 <?php 
 $json_url = "https://alpha-api.app.net/stream/0/posts/tag/mondaynightdanceparty";
 $json = file_get_contents($json_url);
@@ -20,43 +21,76 @@ $post_array_size = sizeof( $post_array );
 //counter
 
 $cnt = 0;
+// Connect to DB 
+$con = mysql_connect("xxxxxxx","xxxxxx","xxxxxxx");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }else{
+
+  	echo "Connection Successful!!!!!";
+  }
+
 
 //while we still have items left to process...keep going.
 while ( $cnt < $post_array_size )
 {
   //Get Mentions Array
 	$mentions_array = $post_array[$cnt]["entities"]["mentions"];
+
+	$mentions_array1 = $post_array[$cnt]["entities"]["mentions"];
+
+
 	//Say who the user is
-	echo "User: " . $post_array[$cnt]["user"]["username"] . "<br>";
-	
+	$user = $post_array[$cnt]["user"]["username"];
+
+	$result = mysql_query("SELECT Username FROM Account_info WHERE Username = '$user'");
+
+     // row not found, do stuff...
+	//echo "UsEr: " . $user;
+
+
+//inserting name into db
+	mysql_select_db("Currentcy", $con);
+
 	//See how many items we have and go through them if there's some.
  	$mentions_size = sizeof( $mentions_array );
 	if ( $mentions_size != 0 )
 	{
+
+		
+		
 		$mention_count = 0;
 		while ( $mention_count < $mentions_size   )
 		{
 				$mentioned_name = $mentions_array[$mention_count]["name"] ;
 				if ( $mentioned_name )
 				{
+
+					if(mysql_num_rows($result) != 0) {
 					echo "Mentioned:  " . $mentioned_name . "<br>";
-					
+
+
+					mysql_query("INSERT INTO Mentions (User,Mentioned) VALUES ('$user','$mentioned_name')");
+                    
 				}
+			}
+
 				$mention_count++;
 			}
 	}
-		
+
 	echo "<hr>";
-	
-	
-	
+
+
+
 	$cnt++;											  
-		
-		
-	
+
+
+
 	}
 
 
-
+mysql_close($con);
 
 ?>
